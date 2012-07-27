768e2ce992a2f9162d84f38115d8365e523b3e3d
Gabriel M. Beddingfield <gabrbedd@ti.com>
2012-07-26 20:17:00 -0500

    audio: Add a separate HAL for HDMI audio
    Creating an audio HAL for HDMI because:
    
      * The HDMI card is already a separate card from
        the OMAP ABE audio.
    
      * The needs of HDMI are getting more specialized
        (e.g. things like multichannel audio, bitstream
        audio, etc.)
    
      * Things in the OMAP ABE HAL are getting a bit
        cramped and complex, and it would be better to
        reduce the complexity rather than add to it.
    
    This version is provides the same functionality as audio_hw.c
    
    Change-Id: Ifcf1cca4f7f49bcabc14c20105a9ddc40f2e33e3
    Signed-off-by: Gabriel M. Beddingfield <gabrbedd@ti.com>

--- audio/Android.mk
+++ audio/Android.mk
@@ -14,6 +14,10 @@
 
 LOCAL_PATH := $(call my-dir)
 
+###
+### OMAP ABE AUDIO HAL
+###
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := audio.primary.p760
@@ -29,3 +33,25 @@ LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+
+###
+### OMAP HDMI AUDIO HAL
+###
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := audio.hdmi.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_SRC_FILES := hdmi_audio_hw.c
+
+LOCAL_C_INCLUDES += \
+	external/tinyalsa/include \
+	system/media/audio_utils/include \
+	system/media/audio_effects/include
+LOCAL_SHARED_LIBRARIES := liblog libcutils libtinyalsa libaudioutils libdl
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
