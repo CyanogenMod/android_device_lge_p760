5c8707c5b30e96a26d42d3d8f082b923631d8561
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-03-14 01:40:55 +0000

    Update desc/fingerprint from JB release

--- cm.mk
+++ cm.mk
@@ -17,7 +17,7 @@ PRODUCT_RELEASE_NAME := OptimusL9
 PRODUCT_VERSION_DEVICE_SPECIFIC :=
 -include vendor/cm/config/common_versions.mk
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u2_open_eu BUILD_FINGERPRINT=lge/u2_open_eu/u2:4.0.4/IMM76L/P76010a.48125853:user/release-keys PRIVATE_BUILD_DESC="u2_open_eu-user 4.0.4 IMM76L P76010a.48125853 release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=u2_open_eu BUILD_FINGERPRINT=lge/u2_open_eu/u2:4.1.2/JRO03L/P76020b.4DAAAC8F:user/release-keys PRIVATE_BUILD_DESC="u2_open_eu-user 4.1.2 JRO03L P76020b.4DAAAC8F release-keys"
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := p760
