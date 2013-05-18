CyanogenMod/android_device_lge_p880

minui: Use backlight control instead of FB blank/unblank
Reduce artifacting caused by blank/unblank and just manage the
display's power instead

BoardConfig.mk
@@ -76,4 +76,5 @@ BOARD_EGL_NEEDS_LEGACY_FB := true
 ## Radio fixes
 BOARD_RIL_CLASS := ../../../device/lge/p880/ril/
 
-
+BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p880/recovery-gfx.c
+BOARD_CHARGER_ENABLE_SUSPEND := true

CyanogenMod/android_device_lge_p760

BoardConfig.mk
@@ -91,3 +91,6 @@ TARGET_KERNEL_MODULES := KERNEL_SGX_MODULES
 BOARD_RIL_CLASS := ../../../device/lge/p760/ril/
 
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p760/bluetooth
+
+BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p760/recovery-gfx.c
+BOARD_CHARGER_ENABLE_SUSPEND := true