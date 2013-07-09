f9275bc5e3a9c0954f12ce3477ec01187912d60d
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-09 01:12:10 +0100

    radio: Split earpiece/speaker call paths
    These apparently need different values in order to work properly
    
    Change-Id: I5c3805ba4996782ffd095279c6860fc85eac8138

--- audio/audio_hw.c
+++ audio/audio_hw.c
@@ -307,7 +307,7 @@ struct route_setting defaults[] = {
     },
     {
         .ctl_name = MIXER_CAPTURE_VOLUME,
-        .intval = DB_TO_CAPTURE_VOLUME(30),
+        .intval = DB_TO_CAPTURE_VOLUME(24),
     },
     {
         .ctl_name = MIXER_SDT_UL_VOLUME,
@@ -886,6 +886,12 @@ static void set_input_volumes(struct omap_audio_device *adev, int main_mic_on,
         volume = DB_TO_ABE_GAIN(main_mic_on ? VOICE_CALL_MAIN_MIC_VOLUME :
                 (headset_mic_on ? VOICE_CALL_HEADSET_MIC_VOLUME :
                 (sub_mic_on ? VOICE_CALL_SUB_MIC_VOLUME : 0)));
+
+        for (channel = 0; channel < 2; channel++) {
+            mixer_ctl_set_value(mixer_get_ctl_by_name(adev->mixer,
+                MIXER_CAPTURE_VOLUME), channel,
+                (main_mic_on ? DB_TO_CAPTURE_VOLUME(18) : DB_TO_CAPTURE_VOLUME(24)));
+        }
     } else if (adev->active_input) {
         /* determine input volume by use case */
         switch (adev->active_input->source) {
@@ -2666,11 +2672,15 @@ static int adev_set_voice_volume(struct audio_hw_device *dev, float volume)
     LOGFUNC("%s(%p, %f)", __FUNCTION__, dev, volume);
     adev->voice_volume = volume;
 
-    mixer_ctl_set_value(mixer_get_ctl_by_name(adev->mixer,
-                ((adev->devices.out_devices & AUDIO_DEVICE_OUT_EARPIECE) ? 
-                    MIXER_DL1_VOICE_PLAYBACK_VOLUME : 
-                    MIXER_DL2_VOICE_PLAYBACK_VOLUME)), 0, 
-                 (MIXER_ABE_GAIN_0DB-20) + (30*volume));
+    if (adev->devices.out_devices & AUDIO_DEVICE_OUT_EARPIECE) {
+        mixer_ctl_set_value(mixer_get_ctl_by_name(adev->mixer,
+                    MIXER_DL1_VOICE_PLAYBACK_VOLUME), 0,
+            (MIXER_ABE_GAIN_0DB-40) + (35*volume));
+    } else {
+        mixer_ctl_set_value(mixer_get_ctl_by_name(adev->mixer,
+                    MIXER_DL2_VOICE_PLAYBACK_VOLUME), 0,
+            (MIXER_ABE_GAIN_0DB-20) + (30*volume));
+    }
 
     return 0;
 }
