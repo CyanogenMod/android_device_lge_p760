c0c923e62c6577553637e7f0314b65cc3b33dbe1
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-07-17 11:49:03 +0100

    selinux: Add device-specific rules
    Change-Id: I69952d49ef0b48d634debc5ac5bc7c2eeea23903

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,3 +95,16 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/p760/bluetooth
 
 BOARD_CUSTOM_GRAPHICS := ../../../device/lge/p760/recovery-gfx.c
 BOARD_CHARGER_ENABLE_SUSPEND := true
+
+ifeq ($(HAVE_SELINUX),true)
+
+BOARD_SEPOLICY_DIRS := \
+    device/lge/p760/selinux
+
+BOARD_SEPOLICY_UNION := \
+    file_contexts \
+    pvrsrvinit.te \
+    device.te \
+    domain.te
+
+endif
